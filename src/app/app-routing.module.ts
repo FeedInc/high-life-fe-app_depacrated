135aa434c8d5b99edbe1d3ca89ef2d26fb5d84aa
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2022-05-06 21:07:26 -0500

    feat: add login from login-branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileStudentComponent } from './views/profile-student/profile-student.component'
 import { ProfileCoachComponent } from './views/profile-coach/profile-coach.component'
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "profile-student", component: ProfileStudentComponent },
-  { path: "profile-coach", component: ProfileCoachComponent }
+  { path: "profile-coach", component: ProfileCoachComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
